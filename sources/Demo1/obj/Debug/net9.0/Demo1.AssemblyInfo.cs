1dc94cab20c89d3951350d06f96cdd9c2927d3f9
LernoutNoah <noah.lernout@student.howest.be>
2025-03-31 19:01:04 +0200

    fix

--- sources/Demo1/obj/Debug/net9.0/Demo1.AssemblyInfo.cs
+++ sources/Demo1/obj/Debug/net9.0/Demo1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7c03c6a95f950a8648c4a8a0f5a6f034d969552")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
